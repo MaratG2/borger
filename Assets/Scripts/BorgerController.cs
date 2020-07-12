18b3a4c5ff1ca03a586af09354037aa162d507aa
Marat <maratg2develop@gmail.com>
2020-07-12 17:13:06 +0400

    Fry bars base feature
    Need to make an input

--- Assets/Scripts/BorgerController.cs
+++ Assets/Scripts/BorgerController.cs
@@ -1,15 +1,20 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class BorgerController : MonoBehaviour
 {
     //variables
+    [Range(0.1f, 15f)][SerializeField] float frySpeed = 5f;
     float hpDown = 0f;
     float hpUp = 0f;
 
     bool upSide;
     bool isFrying;
+
+    public Image barDown;
+    public Image barUp;
 
     private void OnCollisionEnter(Collision collision)
     {
@@ -29,10 +34,17 @@ public class BorgerController : MonoBehaviour
         if(isFrying)
         {
             if (upSide)
-                hpUp += Time.deltaTime;
+                hpUp += Time.deltaTime * frySpeed;
             else
-                hpDown += Time.deltaTime;
+                hpDown += Time.deltaTime * frySpeed;
         }
+        if (hpUp > 100f)
+            hpUp = 100f;
+        if (hpDown > 100f)
+            hpDown = 100f;
+
+        barDown.fillAmount = hpDown / 100f;
+        barUp.fillAmount = hpUp / 100f;
     }
 
     public void ChangeSide(bool newSide)
