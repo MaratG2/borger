451d064328c95814a28f9283b6f2123affe281c8
Marat <maratg2develop@gmail.com>
2020-07-12 16:57:28 +0400

    Frying
    Seems to be working, next is UI and swipe input

---
+++ Assets/Scripts/BorgerController.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BorgerController : MonoBehaviour
+{
+    //variables
+    float hpDown = 0f;
+    float hpUp = 0f;
+
+    bool upSide;
+    bool isFrying;
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        isFrying = true;
+    }
+    private void OnCollisionStay(Collision collision)
+    {
+        isFrying = true;
+    }
+    private void OnCollisionExit(Collision collision)
+    {
+        isFrying = false;
+    }
+
+    private void Update()
+    {
+        if(isFrying)
+        {
+            if (upSide)
+                hpUp += Time.deltaTime;
+            else
+                hpDown += Time.deltaTime;
+        }
+    }
+
+    public void ChangeSide(bool newSide)
+    {
+        upSide = newSide;
+    }
+}
