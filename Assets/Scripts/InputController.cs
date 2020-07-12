5b6bd5042ce7cbe0eaa5041e3bfd9573c9cb1117
Marat <maratg2develop@gmail.com>
2020-07-12 17:56:33 +0400

    Input done
    Need win-lose implementation within the bars

--- Assets/Scripts/InputController.cs
+++ Assets/Scripts/InputController.cs
@@ -1,18 +1,42 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using UnityEditor;
 using UnityEngine;
 
 public class InputController : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    [SerializeField] Rigidbody cubeRB;
 
-    // Update is called once per frame
-    void Update()
+    [SerializeField] float forceFlip = 1f;
+    [SerializeField] float forceAngleFlip = 1f;
+    Vector2 startPos;
+    Vector2 endPos;
+    float startTime;
+    float endTime;
+
+    private void Update()
     {
-        
+        if (Input.touches.Length > 0)
+        {
+            Touch t = Input.GetTouch(0); //Get first touch, doesn't matter how many there are
+            if (t.phase == TouchPhase.Began)
+            {
+                startPos = t.position;
+                startTime = Time.time;
+            }
+            if(t.phase == TouchPhase.Ended)
+            {
+                endPos = t.position;
+                endTime = Time.time;
+
+                if (endTime - startTime < 1f)
+                {
+                    cubeRB.AddForce(0f, forceFlip * (endPos.y - startPos.y) * (endPos - startPos).magnitude * (1 - (endTime - startTime)), 0f);
+                    cubeRB.AddTorque(forceAngleFlip * (endPos.y - startPos.y), 0f, 0f);
+                    startTime = 0f;
+                    endTime = 0f;
+                }
+            }
+        }
     }
 }
