7596d807d73f8f79adb871b2323b82952f22fc8e
Marat <maratg2develop@gmail.com>
2020-07-14 20:38:40 +0400

    Revert "Revert "Win/lose conditions working""
    This reverts commit 491dfd5580261b3a2226d02b069884d3442f2aec.

--- Assets/Scripts/BorgerController.cs
+++ Assets/Scripts/BorgerController.cs
@@ -6,7 +6,12 @@ using UnityEngine.UI;
 public class BorgerController : MonoBehaviour
 {
     //variables
+    [Tooltip("0 - normal, -1 - lost, 1 - won")][SerializeField] int gameCondition = 0; 
     [Range(0.1f, 15f)][SerializeField] float frySpeed = 5f;
+
+    [Range(0f, 1f)] [SerializeField] float minNeededFry = 0.7f;
+    [Range(0f, 1f)] [SerializeField] float maxNeededFry = 0.85f;
+
     float hpDown = 0f;
     float hpUp = 0f;
 
@@ -31,13 +36,14 @@ public class BorgerController : MonoBehaviour
 
     private void Update()
     {
-        if(isFrying)
+        if (isFrying && gameCondition == 0)
         {
             if (upSide)
                 hpUp += Time.deltaTime * frySpeed;
             else
                 hpDown += Time.deltaTime * frySpeed;
         }
+
         if (hpUp > 100f)
             hpUp = 100f;
         if (hpDown > 100f)
@@ -45,10 +51,19 @@ public class BorgerController : MonoBehaviour
 
         barDown.fillAmount = hpDown / 100f;
         barUp.fillAmount = hpUp / 100f;
+
+        if (hpUp / 100f > maxNeededFry || hpDown / 100f > maxNeededFry)
+            gameCondition = -1;
+        
     }
 
     public void ChangeSide(bool newSide)
     {
         upSide = newSide;
+
+        if (hpUp / 100f >= minNeededFry && hpUp / 100f <= maxNeededFry && hpDown / 100f >= minNeededFry && hpDown / 100f <= maxNeededFry)
+        {
+            gameCondition = 1;
+        }
     }
 }
