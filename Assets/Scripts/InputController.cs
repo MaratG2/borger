cdbd5e5c82f65e16b390395ec48cfcd981031ee6
Marat <maratg2develop@gmail.com>
2020-07-16 17:49:44 +0400

    New input by tap
    Fully working, 0.9f seconds between taps

--- Assets/Scripts/InputController.cs
+++ Assets/Scripts/InputController.cs
@@ -8,15 +8,23 @@ public class InputController : MonoBehaviour
     [SerializeField] Rigidbody cubeRB;
     [SerializeField] BorgerController borger;
 
-    [SerializeField] float forceFlip = 1f;
-    [SerializeField] float forceAngleFlip = 1f;
+    [SerializeField] float forceFlip = 0.0001f; //were 0.0001 with an old input
+    [SerializeField] float forceAngleFlip = 1f; //were 1 with an old input
+
+    float timeToFlip = 0.9f;
+    float timerToFlip = 0f;
+    bool ableToFlip = true;
+
+    /* //Old Input variables
     Vector2 startPos;
     Vector2 endPos;
     float startTime;
     float endTime;
-
+    */
     private void Update()
     {
+        //LEGACY INPUT
+        /* 
         if (Input.touches.Length > 0)
         {
             Touch t = Input.GetTouch(0); //Get first touch, doesn't matter how many there are
@@ -37,6 +45,24 @@ public class InputController : MonoBehaviour
                     startTime = 0f;
                     endTime = 0f;
                 }
+            }
+        }*/
+
+        if (timerToFlip < timeToFlip)
+            timerToFlip += Time.deltaTime;
+        else
+            ableToFlip = true;
+
+        if(Input.touches.Length > 0)
+        {
+            Touch t = Input.GetTouch(0);
+
+            if(t.phase == TouchPhase.Began && borger.isFrying && ableToFlip)
+            {
+                cubeRB.AddForce(0f, forceFlip, 0f);
+                cubeRB.AddTorque(forceAngleFlip, 0f, 0f);
+                timerToFlip = 0f;
+                ableToFlip = false;
             }
         }
     }
